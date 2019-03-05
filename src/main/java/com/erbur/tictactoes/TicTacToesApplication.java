297cf09ab8efcd8be44c03e57bc02a042241bf67
Erik Burnell <erik.d.burnell@gmail.com>
2019-03-05 17:48:55 +0100

    Setup TicTacToes Spring Boot Dependencies.

---
+++ src/main/java/com/erbur/tictactoes/TicTacToesApplication.java
@@ -0,0 +1,13 @@
+package com.erbur.tictactoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicTacToesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicTacToesApplication.class, args);
+	}
+
+}
